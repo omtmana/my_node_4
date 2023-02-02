e54df52b838fbe494815103d968126ea70618693
Olivia <oliviatmanalastas@gmail.com>
2023-02-02 10:57:53 -0500

    trial

--- starter/app.js
+++ starter/app.js
@@ -1,43 +1,47 @@
-// Step 1: Import express module
+// Step 1: import packages
 const express = require('express')
 const app = express()
 const fs = require('fs')
 
-// Step 3a: Create a var that reads the file
+// Middleware to be able to post data
+app.use(express.json())
+
+// Step 3: Read a file at the top level
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-// Step 3: Create routes and route handlers
-// GET method route handler
-const getAllRoutes = (req, res) => {
-   res.status(200).json({
-      status: 'success', data: { tours }
-   })
-}
+// Step 4: Creating our routes
+// GET method
+app.get('/tours', (req, res) => {
+   res.status(200).json({ tours })
+})
 
-// GET with params
-const getTour = (req, res) => {
+//  GET with parameters method
+app.get('/tours/:id', (req, res) => {
    const id = req.params.id * 1
-   if (id > tours.length) {
-      return res.status(404).json({
-         status: 'Fail',
-         message: 'Not found'
-      })
+   if (id > tours.length - 1) {
+      return res.status(404).json({ status: 'fail', message: 'ID not found' })
    }
    const tour = tours.find(el => el.id === id)
 
-   res.status(200).json({status: 'success', data: {
-      tour
-   }})
-}
+   res.status(200).json({ tour })
+})
+
+// POST method
+app.post('/tours', (req, res) => {
+   // adding a new id at the end of the json file
+   const newId = tours[tours.length - 1].id + 1
+   const newTour = Object.assign({ id: newId }, req.body)
+   tours.push(newTour)
+
+   // Persist by writing the file to json
+   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+      res.status(200).json({ data: { tour: newTour } })
+   })
+})
 
 
-// Routes
-app.get('/tours', getAllRoutes)
-app.get('/tours/:id', getTour )
-
-
-// Step 2: Set up/Create/Start Server
+// Step 2: Create/start a server
 const port = 3000
 app.listen(port, () => {
-   console.log(`Running on port ${port}...`)
+   console.log(`Server is running on port: ${port}`)
 })
